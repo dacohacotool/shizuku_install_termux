a0f84cbbd697ac279c880bd0ae48f0a3da4d5c73
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2024-09-15 12:20:48 +0700

    Add files via upload

---
+++ shizuku.sh
@@ -0,0 +1,4 @@
+curl "https://firebasestorage.googleapis.com/v0/b/chromium-a120a.appspot.com/o/termux%2Frish?alt=media&token=ea22ed0c-6312-4c95-9121-d7e5392d3e31" -o rish
+curl "https://firebasestorage.googleapis.com/v0/b/chromium-a120a.appspot.com/o/termux%2Frish_shizuku.dex?alt=media&token=4e72a95a-57fc-4ce8-9cf0-5afb2342326c" -o rish_shizuku.dex
+chmod +rwx rish
+chmod +rwx rish_shizuku.dex
