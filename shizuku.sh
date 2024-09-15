9eee2cc632d916fb7731fccf34a11fd9bf6a20d9
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2024-09-15 12:28:25 +0700

    Add files via upload

--- shizuku.sh
+++ shizuku.sh
@@ -1,4 +1,4 @@
-curl "https://github.com/dacohacotool/shizuku_install_termux/raw/main/rish" -o rish
-curl "https://github.com/dacohacotool/shizuku_install_termux/raw/main/rish_shizuku.dex" -o rish_shizuku.dex
+curl "https://raw.githubusercontent.com/dacohacotool/shizuku_install_termux/refs/heads/main/rish" -o rish
+curl "https://raw.githubusercontent.com/dacohacotool/shizuku_install_termux/refs/heads/main/rish_shizuku.dex" -o rish_shizuku.dex
 chmod +rwx rish
 chmod +rwx rish_shizuku.dex
